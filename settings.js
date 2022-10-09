ad90f5bde6dc681fd554165aab9c53ec2b7463ee
Yoshitha0iijj <114879837+Yoshitha0iijj@users.noreply.github.com>
2022-10-10 00:38:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,17 +36,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94775013191'] //ur owner number
+global.ownername = "ﾘの丂んﾉｲんﾑ りﾉﾚﾶﾉｲん" //ur owner name
+global.ytname = "YT: Y_D Music" //ur yt chanel name
+global.socialm = "GitHub: yoshitha-MD GANAG" //ur github or insta name
+global.location = "Sri Lanka, Galle, Poddala" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['94775013191']
+global.ownertag = '94775013191' //ur tag number
+global.botnumber = '94775013191' //Add your Bot Number For Log
+global.botname = 'YOSᕼITᕼᗩ-ᗰᗪ' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
@@ -54,13 +54,20 @@ global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
 global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.author = "Yoshitha-MD" //ur sticker watermark author
+global.footer = "ɯԋαƚʂαρρ υʂҽɾ Ⴆσƚ ƈɾҽαƚҽԃ Ⴆყ Yσʂԋιƚԋα"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "Yσʂԋιƚԋα-Mԃ bot alive now...
+                    type.menu to get command...
+		    money can't get happy මචන්...
+                    it is what it is😂...
+                    mk ඉතින් ඔයගොල්ලො...
+                    මොකද්ද දැන් මගෙන් වෙන්න ඕනෙ🤞...
+		    how can i help you😈...
+		    whatsapp user bot created by yoshitha dilmith"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -96,16 +103,16 @@ global.antitags = true
 global.prefa = ['','!','.']
 global.sp = '⭔'
 global.mess = {
-    success: '✉️✏️ _Done_',
-    admin: '✉️✏️ _This Feature Is Only For Admin!_',
-    botAdmin: '✉️✏️ _Bot Must Be Admin First!_',
-    owner: '✉️✏️ _This Feature Is Only For Owner!_',
-    group: '✉️✏️ _Feature Used Only For Groups!_',
-    private: '✉️✏️ _Features Used Only For Private Chat!_',
-    bot: '✉️✏️ _This Feature Is Only For Bot_',
-    wait: '✉️✏️ _Processing Please Wait..._',
-    linkm: '📧✏️ _Where is the link?_',
-    error: '✉️🤕 ```Error!!```',
+    success: '🤴👈... _Done_',
+    admin: '🤴👈... _This Feature Is Only For Admin!_',
+    botAdmin: '🤴👈... _Bot Must Be Admin First!_',
+    owner: '🤴👈... _This Feature Is Only For Owner!_',
+    group: '🤴👈... _Feature Used Only For Groups!_',
+    private: '🤴👈... _Features Used Only For Private Chat!_',
+    bot: '🤴👈... _This Feature Is Only For Bot_',
+    wait: '🤴👈... _Processing Please Wait..._',
+    linkm: '🤴👈... _Where is the link?_',
+    error: 'fuck 😢```Error!!```',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
